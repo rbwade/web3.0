1e2b0e23da272d1899bc2f33e5f44ab91a90250c
rbwade <you@rushikeshwade@hotmail.com>
2019-02-16 16:48:47 +0530

    Deployed to Rinkby

--- deploy.js
+++ deploy.js
@@ -12,5 +12,12 @@ const web3 = new Web3(provider);
 const deploy = async() =>{
   const accounts = await web3.eth.getAccounts();
   console.log(accounts);
+  console.log('Contract is deployed by the Manager with address',accounts[0]);
+
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+  .deploy({data:'0x' + bytecode})
+  .send({gas:'2000000',from:accounts[0]})
+  console.log('Contract deployed to address', result.options.address);
+  // 0x7F801A10C681bE1155c17aCaaaaE018AA2f9a1BB
 }
 deploy();
--- lottery.js
+++ lottery.js
@@ -1,5 +1,5 @@
 import web3 from './web3.js';
-const address ='';
+const address ='0x7F801A10C681bE1155c17aCaaaaE018AA2f9a1BB';
 const abi =[{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"participants","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"manager","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"pickWinner","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[],"name":"enterLottery","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"}];
 
 export default new web3.eth.Contract(abi,address);
