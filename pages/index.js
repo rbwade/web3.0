5588706212cd1cef5c4c85b7d49f6a9f705a9639
rbwade <you@rushikeshwade@hotmail.com>
2019-02-16 13:51:20 +0530

    Settingup Pages with next

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import React ,{Component} from 'react';
+
+class Lottery extends Component{
+  render(){
+    return(
+      <h1>Decentralized Lottery App</h1>
+    )
+  }
+}
+
+export default Lottery;
