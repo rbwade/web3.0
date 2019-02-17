612182752460322272384b5c74d9f6a989570aea
rbwade <you@rushikeshwade@hotmail.com>
2019-02-17 07:40:15 +0530

    Adding Ether to Contract

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ class Lottery extends Component{
     super(props);
     this.state={
       manager:'',
-      participate_amount:'0.5',
+      participate_amount:'0.01',
       message:'',
       total_amount:''
     }
@@ -20,14 +20,32 @@ class Lottery extends Component{
     const total_amount = await web3.eth.getBalance(lottery.options.address);
     this.setState({total_amount : total_amount});
   }
+
+  onSubmit = async(event)=>{
+    event.preventDefault();
+    const accounts = await web3.eth.getAccounts();
+      if(this.state.participate_amount< 0.01){
+      return aleert("Amount is less than 0.01 please enter bigger amount");
+    }
+    this.setState({message : 'please wait ....'});
+    const enter_lottery = await lottery.methods.enterLottery().send({
+      from: accounts[0],
+      value:web3.utils.toWei(this.state.participate_amount,'ether')
+    });
+    this.setState({message: "You have been added to lottery"});
+  }
   render(){
     return(
       <div>
-        <h1>Total Lottery Pool is {this.state.total_amount} </h1>
-        <form>
-           <input placeholder = "0.5" />
+        <h1>Total Lottery Pool is {web3.utils.fromWei(this.state.total_amount,'ether')} </h1>
+        <form onSubmit = {this.onSubmit}>
+           <input value = {this.state.participate_amount} onChange = {event => this.setState({
+             participate_amount : event.target.value
+           })}
+           />
            <button type="submit">Participate</button>
         </form>
+         <p>{this.state.message} </p>
          <hr /> <br /> <hr />
          <p> The manager of the lottery decentralized app is {this.state.manager} </p>
          <button>Pick Winner</button>
